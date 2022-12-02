3ea5168271087759218934abe2773ba6ea6e4f12
Vamik76 <merts_v@mail.ru>
2022-12-02 21:47:17 +1000

    Lesson1C#HW_task4_end

--- TASK4/Program.cs
+++ TASK4/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Ведите три числа: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+int numberС = Convert.ToInt32(Console.ReadLine());
+if (numberA>numberB)
+{
+    if (numberA>numberС)
+    {
+        Console.Write("Max: ");
+        Console.Write(numberA);
+    }
+
+    else
+    {
+        if (numberС>numberB)
+        {
+            Console.Write("Max: ");
+            Console.Write(numberС);
+        }
+        else
+        {
+            Console.Write("Max: ");
+            Console.Write(numberB);
+        }
+    }
+    
+}
+else
+{
+    if (numberB>numberС)
+    {
+        Console.Write("Max: ");
+        Console.Write(numberB);
+    }
+    else
+    {
+        Console.Write("Max: ");
+        Console.Write(numberС);
+    }
+}
